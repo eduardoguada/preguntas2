aa5feeb66b007b431d2868d3bc6e455169f502c7
Eduardo Guada <eduardoguada@gmail.com>
2024-07-29 13:40:03 +0200

    Update db.php

--- config/db.php
+++ config/db.php
@@ -2,14 +2,8 @@
 
 return [
     'class' => 'yii\db\Connection',
-    //'dsn' => 'mysql:host=localhost;dbname=yii2basic',
     'dsn' => 'mysql:host=localhost;dbname=preguntas',
     'username' => 'root',
     'password' => 'qwerty',
     'charset' => 'utf8',
-
-    // Schema cache options (for production environment)
-    //'enableSchemaCache' => true,
-    //'schemaCacheDuration' => 60,
-    //'schemaCache' => 'cache',
 ];
